3a50f3f358b2e32cf26d21b2e39fa5f852de9ff5
AndrewTechTips <andreww.steel69@gmail.com>
2026-05-03 21:14:52 +0300

    add backend functionality to fetch weather data from OpenWeatherMap API

---
+++ backend.py
@@ -0,0 +1,14 @@
+import requests
+
+API_KEY = "c8899ac16682f96eb396c7adddaa527c"
+
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
